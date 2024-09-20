b08d1ee9a31835e3c3a7ba64b6ac116f0cbe916a
Rajat <68rajat68@gmail.com>
2024-09-20 23:51:23 +0530

    Commit_20_09
    Edit UI

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -40,14 +40,16 @@ const Noteitem = (props) => {
 
         <div className='col-md-3'>
             <div className="card my-3 position-relative">
-                <span className="badge badge-dark position-absolute">{note.tag}</span>
+                <span className="badge badge_01 badge-dark ">{note.tag}</span>
+                <span className="badge badge_02 badge-dark "><i className="fa-regular fa-trash-can mx-1 circle_back" onClick={() => { deleteNote(note._id); props.showAlert("Deleted Successfully", "success") }}></i>
+                <i className="fa-regular fa-pen-to-square mx-1 circle_back" onClick={() => { updateNote(note) }}></i></span>
                 <div className="card-body">
                     <div className='d-flex align-items-center justify-content-between'>
                         <h5 className="card-title">{note.title}</h5>
-                        <div className='d-flex align-items-center'>
+                        {/* <div className='d-flex align-items-center'>
                             <i className="fa-regular fa-trash-can mx-2" onClick={() => { deleteNote(note._id); props.showAlert("Deleted Successfully", "success") }}></i>
                             <i className="fa-regular fa-pen-to-square mx-2" onClick={() => { updateNote(note) }}></i>
-                        </div>
+                        </div> */}
                     </div>
                     <p className="card-text">{note.description}</p>
                 </div>
